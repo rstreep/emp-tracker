8cffd4951ef1f55cf3e4916c1bb2761156ee8cf7
rstreep <127055958+rstreep@users.noreply.github.com>
2023-08-19 13:40:08 -0400

    finalized seed.sql file

---
+++ db/seed.sql
@@ -0,0 +1,30 @@
+INSERT INTO departments (id, name)
+VALUES 
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+
+INSERT INTO roles (title, department_id, salary)
+VALUES 
+    ('Sales Lead', 1, 100000),
+    ('Salesperson', 1, 80000),
+    ('Lead Engineer', 2, 150000),
+    ('Software Engineer', 2, 120000),
+    ('Account Manager', 3, 160000),
+    ('Accountant', 3, 125000),
+    ('Legal Team lead', 4, 250000),
+    ('Lawyer', 4, 190000);
+
+
+INSERT INTO employeees (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('Nellie', 'Stinky', 1, NULL),
+    ('Scarlet', 'Bagonias', 2, 1),
+    ('Chris', 'Topher', 3, NULL),
+    ('Rich', 'Iana', 4, 3),
+    ('Dev', 'Iana', 5, NULL),
+    ('Luna', 'Tuna', 6, 5),
+    ('Kayla', 'Smells', 7, NULL),
+    ('Janine', 'Eatshorts', 8, 7);
